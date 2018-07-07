b19ef0be3892643c68a179fa7fc712e55700b274
geetika25 <geetika2598@gmail.com>
2018-07-06 21:41:37 -0700

    question_1

---
+++ question_1.py
@@ -0,0 +1,6 @@
+# pandas
+
+#1. Write a Python program to create and display a one-dimensional array-like object containing an array of data using Pandas module.
+import pandas as pd
+ds=pd.Series([4,2,3,4,6])
+print(ds)

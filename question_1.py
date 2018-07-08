2a56437697d91ffcd7fb06d2e585d9b98b63e010
geetika25 <geetika2598@gmail.com>
2018-07-07 21:48:31 -0700

    question.2

--- question_1.py
+++ question_1.py
@@ -4,3 +4,10 @@
 import pandas as pd
 ds=pd.Series([4,2,3,4,6])
 print(ds)
+
+#2. Write a Python program to print the following string in a specific format
+#Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+
+#solution 2:
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n!Up above the world so high,\n\t Like a diamond in the sky.\n Twinkle, twinkle, little star,\n\t How I wonder what you are")
+
